5e50561c4458e54a7432c6c5fff2454307f30f85
MarlonGuevara <141768047+MarlonKuna26@users.noreply.github.com>
2026-04-27 21:55:25 +0000

    feat: ultima actualizacion router

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-import express from 'express'
+import express, {json} from 'express'
+import { movieRouter } from './routes/movies'
 
 const app = express()
 
 const PORT = process.env.PORT ?? 3000
+
+app.use(json())
+app.use('/movies', movieRouter({MovieModel}))
 
 app.listen(PORT, () =>{
     console.log('server listening in PORT: ${PORT}')
